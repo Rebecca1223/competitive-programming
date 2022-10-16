5cf3f705faf4800463b26a6fa6d9dfb469b365c0
Rebecca Lai <rebeccalai2003@gmail.com>
2022-10-16 01:34:23 -0400

    Time: 2 ms (63.02%), Space: 42.4 MB (20.65%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        
+        if (t.length() < s.length()) {
+            return false;
+        }
+        
+        int sIndex = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(sIndex) == t.charAt(i)){
+                sIndex++;
+            }
+            if (sIndex == s.length()) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
