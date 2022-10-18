84e3e4cb83713b937c722b60d3499170fbd0768d
Rebecca Lai <rebeccalai2003@gmail.com>
2022-10-18 13:17:31 -0400

    Time: 0 ms (100.00%), Space: 54.4 MB (43.46%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        return binary(nums, target, len-1, 0);
+    }
+    
+    int binary(int[] nums, int target, int high, int low) {
+        if (low > high) {
+            return -1;
+        } else {
+            int mid = (high + low)/2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                return binary(nums, target, high, mid+1);
+            } else {
+                return binary(nums, target, mid-1, low);
+            }
+        }
+    }
+}
