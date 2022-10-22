de0fdb145a55e079a18694170a4aec83e6fe3506
Rebecca Lai <rebeccalai2003@gmail.com>
2022-10-22 02:44:19 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (12.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -17,19 +17,5 @@ class Solution {
         }
         
         return stairs[n];
-//         if (n < 2) {
-//             return 1;
-//         }
-
-//         int prev1 = 1;
-//         int prev2 = 2;
-
-//         for (int i = 3; i <= n; i++) {
-//             int sum = prev1 + prev2;
-//             prev1 = prev2;
-//             prev2 = sum;
-//         }
-
-//         return prev2;
     }
 }
