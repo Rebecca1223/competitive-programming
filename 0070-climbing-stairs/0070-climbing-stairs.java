ecc3cae3559e82c87c5858ce68b815a65e12077a
Rebecca Lai <rebeccalai2003@gmail.com>
2022-10-22 02:42:41 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (69.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,35 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] stairs = new int[n+1];
+        
         if (n < 2) {
             return 1;
+        } else if (n == 2) {
+            return 2;
         }
-
-        int prev1 = 1;
-        int prev2 = 2;
-
-        for (int i = 3; i <= n; i++) {
-            int sum = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = sum;
+        
+        stairs[0] = 0;
+        stairs[1] = 1;
+        stairs[2] = 2;
+        
+        for (int i = 3; i < n+1; i++) {
+            stairs[i] = stairs[i-1] + stairs[i-2];
         }
+        
+        return stairs[n];
+//         if (n < 2) {
+//             return 1;
+//         }
 
-        return prev2;
+//         int prev1 = 1;
+//         int prev2 = 2;
+
+//         for (int i = 3; i <= n; i++) {
+//             int sum = prev1 + prev2;
+//             prev1 = prev2;
+//             prev2 = sum;
+//         }
+
+//         return prev2;
     }
 }
