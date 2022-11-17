e445b8e85f3c6c91a62eb19a6e6d321a6c10de4a
Rebecca Lai <rebeccalai2003@gmail.com>
2022-11-17 03:13:55 -0500

    Time: 18 ms (35.38%), Space: 55.8 MB (58.88%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        int num = 0, numf = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (freq.containsKey(nums[i])) {
+                freq.put(nums[i], freq.get(nums[i]) + 1);
+            } else {
+                freq.put(nums[i], 1);
+            }
+        }
+        
+        for (int i : freq.keySet()) {
+            if (freq.get(i) > numf) {
+                numf = freq.get(i);
+                num = i;
+            }
+        }
+        
+        return num;
+    }
+}
