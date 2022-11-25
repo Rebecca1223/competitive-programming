7a490f3ff5622f6097ccf05233a6107c1da50c1a
Rebecca Lai <rebeccalai2003@gmail.com>
2022-11-25 01:19:56 -0500

    Time: 15 ms (22.33%), Space: 54.9 MB (15.38%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        
+        for (int i = 0; i < timeSeries.length-1; i++) {
+            if (timeSeries[i+1] - timeSeries[i] < duration) {
+                total += timeSeries[i+1] - timeSeries[i];
+            } else {
+                total += duration;
+            }
+        }
+        
+        total += duration;
+        return total;
+    }
+}
