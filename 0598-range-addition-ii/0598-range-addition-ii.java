209a0b34f88cbe53718d9716332c0edf2d4d07e1
Rebecca Lai <rebeccalai2003@gmail.com>
2022-11-27 02:13:25 -0500

    Time: 2 ms (43.65%), Space: 45.3 MB (6.00%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.java
+++ 0598-range-addition-ii/0598-range-addition-ii.java
@@ -1,8 +1,5 @@
 class Solution {
     public int maxCount(int m, int n, int[][] ops) {
-        int xMin = m;
-        int yMin = n;
-        
         for (int i = 0; i < ops.length; i++) {
             if (ops[i][0] < m) m = ops[i][0];
             if (ops[i][1] < n) n = ops[i][1];
