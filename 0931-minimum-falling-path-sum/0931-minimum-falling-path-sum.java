822fac6d1c6db288c40d009e00059321e51ac659
Rebecca Lai <rebeccalai2003@gmail.com>
2022-12-13 05:45:32 -0500

    Time: 7 ms (55.41%), Space: 48 MB (20.27%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,14 +3,10 @@ import java.util.*;
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         for (int i = 1; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (j == 0) {
-                    matrix[i][j] += Math.min(matrix[i-1][j], matrix[i-1][j+1]);
-                } else if (j == matrix[0].length - 1) {
-                    matrix[i][j] += Math.min(matrix[i-1][j], matrix[i-1][j-1]);
-                } else {
-                    matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i-1][j-1], matrix[i-1][j+1]));
-                }
+            matrix[i][0] += Math.min(matrix[i-1][0], matrix[i-1][1]);
+            matrix[i][matrix.length-1] += Math.min(matrix[i-1][matrix.length-2], matrix[i-1][matrix.length-1]);
+            for (int j = 1; j < matrix.length - 1; j++) {
+                matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i-1][j-1], matrix[i-1][j+1]));
             }
         }
         
