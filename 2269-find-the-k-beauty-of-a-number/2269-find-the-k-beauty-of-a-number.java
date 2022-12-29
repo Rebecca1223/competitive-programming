6e2a9216e2a0536fe76413a1e04aca8da3753321
Rebecca Lai <rebeccalai2003@gmail.com>
2022-12-29 04:33:26 -0500

    Time: 1 ms (92.12%), Space: 39.8 MB (71.92%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -6,7 +6,6 @@ class Solution {
         int divisor = (int)Math.pow(10, (len - k));
         while (num != 0) {
             int temp = num / divisor;
-            System.out.println(num + "/" + divisor + " = " + temp);
             if (temp != 0 && original % temp == 0) count++;
             num %= Math.pow(10, len-1);
             len--;
