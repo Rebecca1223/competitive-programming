72b9f5fd8a5a78e4276a7238d170eff9ad438fdd
Rebecca Lai <rebeccalai2003@gmail.com>
2022-12-30 03:41:35 -0500

    Time: 10 ms (60.69%), Space: 41 MB (93.45%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,14 +1,14 @@
 class Solution {
     public String greatestLetter(String s) {
-        HashSet<Character> seen = new HashSet<Character>();
+        HashSet<Integer> seen = new HashSet<Integer>();
         int max = ' ';
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' && seen.contains((char)(s.charAt(i) + 32))) { //if uppercase
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' && seen.contains((s.charAt(i) + 32))) { //if uppercase
                 if (s.charAt(i) > max) max = s.charAt(i);
-            } else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z' && seen.contains((char)(s.charAt(i) - 32))) { //if lower
+            } else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z' && seen.contains(s.charAt(i) - 32)) { //if lower
                 if (s.charAt(i) - 32 > max) max = s.charAt(i) - 32;
             } else {
-                seen.add(s.charAt(i));
+                seen.add((int)s.charAt(i));
             }
         }
         if (max == ' ') return "";
