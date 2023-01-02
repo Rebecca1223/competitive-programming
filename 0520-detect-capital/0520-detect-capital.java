f8debd52032fc6878f9221dfb74b7b080059d024
Rebecca Lai <rebeccalai2003@gmail.com>
2023-01-02 02:01:34 -0500

    Time: 1 ms (100.00%), Space: 41 MB (69.49%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        return word.matches("[A-Z]*|.[a-z]*");
+        if (word.charAt(0) >= 'A' && word.charAt(0) <= 'Z' && word.length() > 1) { 
+            if (word.charAt(1) < 'a') { // uppercase
+                for (int i = 2; i < word.length(); i++) {
+                    if (word.charAt(i) >= 'a') return false;
+                }
+            } else { // lowercase
+                for (int i = 2; i < word.length(); i++) {
+                    if (word.charAt(i) < 'a') return false;
+                }
+            }
+        } else { // lowercase
+            for (int i = 1; i < word.length(); i++) {
+                if (word.charAt(i) < 'a') return false;
+            }
+        }
+        return true;
     }
 }
