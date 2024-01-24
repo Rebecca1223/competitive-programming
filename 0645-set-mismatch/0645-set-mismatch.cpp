6c5956e3641b62effceb0ec25b4e89cd53d85cd6
Rebecca Lai <rebeccalai2003@gmail.com>
2024-01-23 19:45:26 -0500

    Time: 44 ms (36.40%), Space: 34.4 MB (19.78%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_set<int> seen;
+        vector<int> output;
+        
+        for (auto num : nums) {
+            if (seen.count(num) > 0) {
+                output.push_back(num);
+            } else {
+                seen.insert(num);
+            }
+        }
+        
+        for (int i = 1; i <= nums.size(); i++) {
+            if (seen.count(i) < 1) output.push_back(i);
+        }
+        
+        return output;
+    }
+};
