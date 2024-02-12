0274a9b34fb7ef208fb939a74a5a48de0872a766
Rebecca Lai <rebeccalai2003@gmail.com>
2024-02-12 15:03:51 -0500

    Time: 8 ms (92.56%), Space: 37.7 MB (24.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    void palin(int& maxLen, pair<int, int>& pos, int start, int end, const string s, const int n) {
+        while (true) {
+            if (start < 0 || end >= n || s[start] != s[end]) {
+                if (end - start - 1 > maxLen) {
+                    maxLen = end-start-1;
+                    pos = {start+1, end-1};
+                }
+                break;
+            }
+            start--;
+            end++;
+        }
+    }
+    
     string longestPalindrome(string s) {
         int n = s.length();
         if (n == 1) return s;
@@ -16,37 +30,11 @@ public:
             int len = 0;
             
             if (s[i] == s[i-1]) { // even
-                int end = i;
-                int start = i-1;
-                
-                while (true) {
-                    if (start < 0 || end >= n || s[start] != s[end]) {
-                        if (end - start - 1 > maxLen) {
-                            maxLen = end-start-1;
-                            pos = {start+1, end-1};
-                        }
-                        break;
-                    }
-                    start--;
-                    end++;
-                }
+                palin(maxLen, pos, i-1, i, s, n);
             }
             
             if (s[i] == s[i-2]) { // odd
-                int end = i;
-                int start = i-2;
-                
-                while (true) {
-                    if (start < 0 || end >= n || s[start] != s[end]) {
-                        if (end - start - 1 > maxLen) {
-                            maxLen = end-start-1;
-                            pos = {start+1, end-1};
-                        }
-                        break;
-                    }
-                    start--;
-                    end++;
-                }
+                palin(maxLen, pos, i-2, i, s, n);
             }
         }
         
