39489892d00942eb8c4f7584735ff59479eb8c39
Rebecca Lai <rebeccalai2003@gmail.com>
2024-03-09 03:08:53 -0500

    Time: 63 ms (86.75%), Space: 52.9 MB (51.54%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int one = 0, two = 0;
+        
+        while (one < nums1.size() && two < nums2.size()) {
+            if (nums1[one] == nums2[two]) {
+                return nums1[one];
+            } else if (nums1[one] > nums2[two]) {
+                two++;
+            } else {
+                one++;
+            }
+        }
+        
+        return -1;
+    }
+};
