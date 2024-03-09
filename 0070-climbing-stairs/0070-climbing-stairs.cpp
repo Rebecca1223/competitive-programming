d13bb6a911d9635dd9c263db014a15bf914ea139
Rebecca Lai <rebeccalai2003@gmail.com>
2024-03-09 16:27:45 -0500

    Time: 0 ms (100.00%), Space: 7.4 MB (10.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> ways = {1, 2};
-        
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        for (int i = 2; i < n; i++) {
-            ways.push_back(ways[i-2] + ways[i-1]);
+        if(n==1) return 1;
+        vector<int> dp(n + 1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         
-        return ways[n-1];
+        return dp[n];
     }
 };
