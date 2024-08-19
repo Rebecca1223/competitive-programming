e6ef8f0efdb700d692da5c2c2835eea6be390b78
Rebecca Lai <rebeccalai2003@gmail.com>
2024-08-19 02:10:40 -0700

    Time: 89 ms (61.84%), Space: 16.5 MB (61.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -3,7 +3,7 @@ class Solution:
         steps = [0, 2, 3, 4, 5, 5, 7, 6]
         
         for i in range (9, n+1):
-            for j in range (i-1, -1, -1):
+            for j in range (i//2, -1, -1):
                 if i%j == 0:
                     steps.append(steps[j-1] + i//j)
                     break
