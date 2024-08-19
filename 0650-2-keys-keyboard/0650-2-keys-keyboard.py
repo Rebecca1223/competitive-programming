f78f52820c31464836d30c1bedd8f6c717db8e47
Rebecca Lai <rebeccalai2003@gmail.com>
2024-08-19 02:05:03 -0700

    Time: 266 ms (27.90%), Space: 16.4 MB (99.66%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,10 +1,8 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        num = [1, 2, 3, 4, 5, 6, 7, 8]
         steps = [0, 2, 3, 4, 5, 5, 7, 6]
         
         for i in range (9, n+1):
-            num.append(i)
             for j in range (i-1, -1, -1):
                 if i%j == 0:
                     steps.append(steps[j-1] + i//j)
